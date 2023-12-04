4218c290d78e8dfec6eb24a29c95af226dba6938
Clayton Bailey <cbailey6079@gmail.com>
2023-12-04 00:14:35 -0700

    initial solutions for day 4

--- year2023/src/days/day4.rs
+++ year2023/src/days/day4.rs
@@ -2,17 +2,86 @@ use crate::{utils, Day};
 
 pub struct Day4 {}
 
+struct Card {
+    copies: u32,
+    winning_numbers: Vec<u8>,
+    current_numbers: Vec<u8>,
+}
+
 impl Day for Day4 {
     fn part1(&self, file: String) -> String {
-        let lines = utils::read_lines(format!("./src/files/day4/{file}.txt").as_str());
+        let cards = get_cards(file);
+        let powpow: u32 = 2;
+        let mut points = 0;
+        let mut winners;
 
-        
-        "".to_string()
+        for card in cards {
+            winners = Vec::new();
+
+            for winner in card.winning_numbers {
+                if card.current_numbers.contains(&winner) {
+                    winners.push(winner);
+                }
+            }
+
+            if winners.len() > 0 {
+                points += powpow.pow(winners.len() as u32 - 1);
+            }
+
+        }
+
+        points.to_string()
     }
 
     fn part2(&self, file: String) -> String {
-        let lines = utils::read_lines(format!("./src/files/day4/{file}.txt").as_str());
-        
-        "".to_string()
+        let mut cards = get_cards(file);
+        let mut total_cards = cards.len();
+        let mut winners;
+
+        for card_index in 0..cards.len() {
+            winners = Vec::new();
+
+            for winner in &cards[card_index].winning_numbers {
+                if cards[card_index].current_numbers.contains(&winner) {
+                    winners.push(winner);
+                }
+            }
+
+            if winners.len() > 0 {
+                for x in 1..winners.len() + 1 {
+                    cards[card_index + x].copies += cards[card_index].copies;
+                    total_cards += cards[card_index].copies as usize;
+                }
+            }
+        }
+
+        total_cards.to_string()
     }
+}
+
+fn get_cards(file: String) -> Vec<Card> {
+    let lines = utils::read_lines(format!("./src/files/day4/{file}.txt").as_str());
+    let mut cards: Vec<Card> = Vec::new();
+
+
+    for line in lines {
+        let card: Vec<&str> = line.split(":").collect(); 
+        let numbers: Vec<&str> = card[1].split(" |").collect();
+
+        let winners = numbers[0].chars()
+            .collect::<Vec<char>>()
+            .chunks(3)
+            .map(|c| c.iter().collect::<String>().trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+
+        let current = numbers[1].chars()
+            .collect::<Vec<char>>()
+            .chunks(3)
+            .map(|c| c.iter().collect::<String>().trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+
+        cards.push(Card {copies: 1, winning_numbers: winners, current_numbers: current});
+    }
+
+    cards
 }
--- year2023/src/tests/day4_tests.rs
+++ year2023/src/tests/day4_tests.rs
@@ -5,7 +5,7 @@ use crate::{days::day4::Day4, Day};
 #[test]
 fn day4_part1_example() {
     let day = Day4 {};
-    let expected = "";
+    let expected = "13";
     let actual = day.part1("test1".to_string());
 
     assert_eq!(actual, expected);
@@ -14,7 +14,7 @@ fn day4_part1_example() {
 #[test]
 fn day4_part1() {
     let day = Day4 {};
-    let expected = "";
+    let expected = "25183";
     let actual = day.part1("input".to_string());
 
     assert_eq!(actual, expected);
@@ -23,7 +23,7 @@ fn day4_part1() {
 #[test]
 fn day4_part2_example() {
     let day = Day4 {};
-    let expected = "";
+    let expected = "30";
     let actual = day.part2("test1".to_string());
 
     assert_eq!(actual, expected);
@@ -32,7 +32,7 @@ fn day4_part2_example() {
 #[test]
 fn day4_part2() {
     let day = Day4 {};
-    let expected = "";
+    let expected = "5667240";
     let actual = day.part2("input".to_string());
 
     assert_eq!(actual, expected);
