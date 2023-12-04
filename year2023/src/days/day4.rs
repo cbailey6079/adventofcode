ce2377771bd26f3a323d8a5eaa06694e40907321
Clayton Bailey <cbailey6079@gmail.com>
2023-12-03 19:35:14 -0700

    correcting the file location for day4

--- year2023/src/days/day4.rs
+++ year2023/src/days/day4.rs
@@ -4,14 +4,14 @@ pub struct Day4 {}
 
 impl Day for Day4 {
     fn part1(&self, file: String) -> String {
-        let lines = utils::read_lines(format!("./src/files/day1/{file}.txt").as_str());
+        let lines = utils::read_lines(format!("./src/files/day4/{file}.txt").as_str());
 
         
         "".to_string()
     }
 
     fn part2(&self, file: String) -> String {
-        let lines = utils::read_lines(format!("./src/files/day1/{file}.txt").as_str());
+        let lines = utils::read_lines(format!("./src/files/day4/{file}.txt").as_str());
         
         "".to_string()
     }
